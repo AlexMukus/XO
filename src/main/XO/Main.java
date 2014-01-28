24017d221a45e78513dd669bb7f06e09ccca612f
Alexandr <lobov.a@techprom.net>
2014-01-28 13:42:39 +0600

    развитие проекта

--- src/main/XO/Main.java
+++ src/main/XO/Main.java
@@ -5,7 +5,10 @@ import main.XO.Field.*;
 public class Main {
     public static void main(String[] args) {
         FieldOut field = new FieldOut();
+
+        field.clearField();
         field.setCellX(1,1);
         field.showField();
+        System.out.print(field.getCell(1,1));
     }
 }
